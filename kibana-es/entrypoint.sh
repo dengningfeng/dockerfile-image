069d4003ce8fd76e81ce8f4a607d3f53245c39c0
dengningfeng <1941455590@qq.com>
2022-10-28 16:40:51 +0800

    :sparkles: 修改日志地址

--- kibana-es/entrypoint.sh
+++ kibana-es/entrypoint.sh
@@ -1,4 +1,4 @@
 #!bin/bash
-nohup /usr/share/kibana/bin/kibana >> /usr/share/kibana/logs/kibana.log 2>&1 &
-nohup /usr/share/elasticsearch/bin/elasticsearch >> /usr/share/elasticsearch/logs/es.log 2>&1  &
+nohup /usr/share/elasticsearch/bin/elasticsearch >> /var/log/es.log 2>&1  &
+nohup /usr/share/kibana/bin/kibana >> /var/log/kibana.log 2>&1 &
 
