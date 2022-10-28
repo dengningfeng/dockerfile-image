3a71748815257ac567bc4a438e58cbf40eaf8a28
dengningfeng <1941455590@qq.com>
2022-10-28 15:43:55 +0800

    :sparkles: 添加namesrv和brokerIP通过环境变量的方式配置

--- rocketmq5/entrypoint.sh
+++ rocketmq5/entrypoint.sh
@@ -1,2 +1,4 @@
+echo namesrvAddr=${NAMESRV_ADDR} >> /usr/local/rocketmq/conf/broker.conf
+echo brokerIP1=${BROKER_IP} >> /usr/local/rocketmq/conf/broker.conf
 nohup sh /usr/local/rocketmq/bin/mqnamesrv > /var/log/rocketmq.d/namesrv.log 2>&1 &
-nohup sh /usr/local/rocketmq/bin/mqbroker -n localhost:9876 autoCreateTopicEnable=true > /var/log/rocketmq.d/broker.log 2>&1 &
+nohup sh /usr/local/rocketmq/bin/mqbroker -n localhost:9876 -c /usr/local/rocketmq/conf/broker.conf autoCreateTopicEnable=true > /var/log/rocketmq.d/broker.log 2>&1 &
